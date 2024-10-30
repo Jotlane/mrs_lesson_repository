d219338490cd4408d9aa63cf42da0fc3601fbde3
peky <ryan_pek@mymail.sutd.edu.sg>
2024-10-30 13:54:13 +0800

    owo

--- testing123.c
+++ testing123.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+typedef struct owo{
+	char *owomessage;
+	int owoint;
+}owo;
+
 int main(void){
 	printf("test\n");
+	owo newowo;
+	newowo.owomessage="owowowowo";
+	newowo.owoint=1;
+	printf("%s\n",newowo.owomessage);
+	return 0;
 }
